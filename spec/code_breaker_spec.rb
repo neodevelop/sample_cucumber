26f1cf1d2f4a14d23aaf650ccdffd0e11f06451a
neodevelop <neodevelop@gmail.com>
2014-03-16 01:27:32 -0600

    refactor de prueba, usando arreglos

--- spec/code_breaker_spec.rb
+++ spec/code_breaker_spec.rb
@@ -1,43 +1,27 @@
 require_relative '../lib/code_breaker'
 
 describe CodeBreaker do
+
   before do
-    @game = CodeBreaker.new("9172")
+    @secret_code = "9172"
+    @game = CodeBreaker.new(@secret_code)
   end
 
-  it "When the secret code is 9172 and try to guess with 9345 the result is 'X' " do
-    r = @game.guessWith("9345")
-    r.should eq("X")
-  end
-
-  it "When the secret code is 9172 and try to guess with 0945 the result is '*' " do
-    r = @game.guessWith("0945")
-    r.should eq("*")
-  end
-
-  it "When the secret code is 9172 and try to guess with 9375 the result is 'XX' " do
-    r = @game.guessWith("9375")
-    r.should eq("XX")
-  end
-
-  it "When the secret code is 9172 and try to guess with 0915 the result is '**' " do
-    r = @game.guessWith("0915")
-    r.should eq("**")
-  end
-
-  it "When the secret code is 9172 and try to guess with 9175 the result is 'XXX' " do
-    r = @game.guessWith("9175")
-    r.should eq("XXX")
-  end
-
-  it "When the secret code is 9172 and try to guess with 2915 the result is '***' " do
-    r = @game.guessWith("2915")
-    r.should eq("***")
-  end
-
-  it "When the secret code is 9172 and try to guess with 9912 the result is 'E' " do
-    r = @game.guessWith("9912")
-    r.should eq("E")
+  [
+    %w( 9345 X ),
+    %w( 0945 * ),
+    %w( 9375 XX ),
+    %w( 0915 ** ),
+    %w( 9175 XXX ),
+    %w( 2915 *** ),
+    %w( 9912 E )
+  ].each do | guess_number, result |
+    it %{ When the secret code is #{@secret_code} 
+          and try to guess with #{guess_number} 
+          the result is '#{result}' } do
+      r = @game.guessWith(guess_number)
+      r.should eq(result)
+    end
   end
 
   it "When I try to guess 2 times and ask for the status game the resul it 'Try again!'" do
